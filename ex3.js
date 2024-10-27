add81cb0f18c718fc3898af706b3b74d7d604ed2
Tarek-Al-Khatib <tarek.al.khatiib@gmail.com>
2024-10-27 14:57:04 +0200

    did the logic and styling behind the christmas tree

--- ex3.js
+++ ex3.js
@@ -2,11 +2,19 @@ const treeHeight = parseInt(
   prompt("Please enter the size of the Christmas tree:")
 );
 var tree = document.getElementById("tree");
-
-for (var i = 1; i <= treeHeight; i++) {
-  var level = " ".repeat(treeHeight - i) + "*" + "|".repeat(i - 1) + "*";
+var line = "|";
+var star = "*";
+var emptySpace = " ";
+for (var i = 1; i < treeHeight; i++) {
+  if (i == 1) {
+    var level = "*";
+    var htmlLevel = document.createElement("div");
+    htmlLevel.innerHTML = `<p class="gold-star">${level}</p>`;
+    tree.appendChild(htmlLevel);
+  }
+  var level =
+    emptySpace.repeat(treeHeight - i) + star + line.repeat(2 * i - 1) + star;
   var htmlLevel = document.createElement("div");
-  htmlLevel.innerHTML =
-    i == 1 ? `<p class="gold-star">${level}</p>` : `<p>${level}</p>`;
+  htmlLevel.innerHTML = `<p>${level}</p>`;
   tree.appendChild(htmlLevel);
 }
